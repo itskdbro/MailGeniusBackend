e8c2bd4b1b7da264622a7bb7045dbe0cb7cb5a5a
Keshav Dhawan <126229636+itskdbro@users.noreply.github.com>
2025-02-08 12:33:45 +0530

    Add files via upload

---
+++ SmartEmailReply/src/main/java/com/SmartEmailReply/SmartEmailReply/SmartEmailReplyApplication.java
@@ -0,0 +1,13 @@
+package com.SmartEmailReply.SmartEmailReply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEmailReplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEmailReplyApplication.class, args);
+	}
+
+}
